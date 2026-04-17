19111558ddaa85bbb15d9e291ac558561d980f00
ERICK ANATANAHEL VELASQUEZ DE LEON <ervel.10.09@gmail.com>
2026-04-17 10:26:07 -0600

    fix: ignore typescript errors in next config

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
-    // 🚩 Esto ignora los errores de tipos para que el build termine
+    // 🚩 Esto ignora los errores de tipos para que el build termine exitosamente en Vercel
     ignoreBuildErrors: true,
   },
   eslint: {
-    // 🚩 Esto ignora advertencias de formato
+    // 🚩 Esto ignora advertencias de formato durante el despliegue
     ignoreDuringBuilds: true,
   },
 };
